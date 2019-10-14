84178b1ff6cc3c643f5faa4f24065addacb28d1a
Shunya <yamasyunizig1216@gmail.com>
2019-10-14 17:38:22 +0900

    Chat space の作成

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,7 +1,25 @@
 <template>
    <div class="chat container">
-      <h2>Chat Room</h2>
-      <h5>Welcome to {{this.name}}</h5>
+
+      <h2 class="titlec">Chat Room</h2>
+      <h5 class="titlelc">Welcome to {{this.name}}</h5>
+
+      <div class="card">
+         <div class="card-content">
+            <ul class="messages">
+               <li>
+                  <span class="m-name">Name</span>
+                  <span class="m-message">Message</span>
+                  <span class="m-time time">time</span>
+               </li>
+            </ul>
+         </div>
+
+         <div class="card-action">
+            <input type="text">
+         </div>
+      </div>
+
    </div>
 </template>
 
@@ -18,6 +36,33 @@ export default {
 </script>
 
 <style>
-
+.titlec{
+   color: #F99273
+}
+.titlelc{
+   color: #E5E0DF
+}
+.m-name{
+   color: #F99273
+}
+.m-message{
+   color: #757575
+}
+.m-time{
+   color: #E5E0DF
+}
+.chat h5{
+   margin-bottom: 70px;
+}
+.chat span{
+   font-size: 1.4em;
+}
+.chat .time{
+   display: block;
+   font-size: 1.2em;
+}
+.chat li{
+   text-align: left
+}
 
 </style>
