5cb30ccd450721e15eb53b856fcbd59b330623bd
Shunya <yamasyunizig1216@gmail.com>
2019-10-15 14:04:01 +0900

    Moment.js を導入して時間の見え方を変えた

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -27,6 +27,8 @@
 <script>
 import NewMessage from '@/components/NewMessage'
 import db from '@/firebase/init'
+import moment from 'moment'
+
 export default {
   name: 'Chat',
   props: ['name'],
@@ -51,7 +53,7 @@ export default {
             id: doc.id,
             name: doc.data().name,
             content: doc.data().content,
-            timestamp: doc.data().timestamp
+            timestamp: moment(doc.data().timestamp).format('lll')
           })
         }
       })
